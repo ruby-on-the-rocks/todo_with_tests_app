d742c0b7d23d7c51864cd8c31e030ff19fe8d747
Peter Jang <peter.x.jang@gmail.com>
2019-04-15 21:00:34 -0500

    Add list model associated with task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_003017) do
+ActiveRecord::Schema.define(version: 2019_04_16_015806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -25,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_003017) do
     t.boolean "favorite"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "list_id"
   end
 
 end
